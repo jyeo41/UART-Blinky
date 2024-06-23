331edf75584ba5c88f8b19b84951160f2b56fefc
Sionnix <jyeo41@gmail.com>
2024-06-23 09:17:47 -0400

    bug(uart_interrupt): Add more debug lines
    Flashing LED directly inside of the if statements in ISR to check the
    code is entering them properly.

--- uart_interrupt.c
+++ uart_interrupt.c
@@ -114,23 +114,27 @@ void UART0_Handler(void)
 {
 	volatile unsigned long txim = UART0_MIS_R & 0x20;	// sanity checking interrupt trigger flag
 	volatile unsigned long rxim = UART0_MIS_R & 0x10;	// sanity checking interrupt trigger flag
-	GPIO_PORTF_DATA_R ^= 0x04;		// debugging to confirm ISR is triggered
+	GPIO_PORTF_DATA_R ^= 0x04;		// set blue LED to confirm ISR is triggered
 	
 	// Receive interrupt triggered
 	// If bit is set in the MIS register AND Receive FIFO is full (data has been received and is ready to be read),
 	// 	put the received data into the rx_ring_buffer
 	if ((UART0_MIS_R & 0x10) && (UART0_FR_R & 0x40))
 	{
+		GPIO_PORTF_DATA_R = 0x02;		// set red LED to confirm it entered the receive interrupt if statement
+		UART0_DR_R = 'R';						// show 'R' on terminal too
 		// Acknowledge receive interrupt
 		uart0_interrupt_clear_receive();
 		
 		// Read the data out from the HW RxFIFO and put it into the rx_ring_buffer
-		ring_buffer_write(&rx_ring_buffer, UART0_DR_R);
+		// ring_buffer_write(&rx_ring_buffer, UART0_DR_R);
 	}
 	// Transmit interrupt triggered
 	// If interrupt bit is set in the MIS register AND Transmit FIFO is empty (UART is ready to send more data)
 	if ((UART0_MIS_R & 0x20) && (UART0_FR_R & 0x80))
 	{
+		GPIO_PORTF_DATA_R = 0x08;		// set green LED to confirm it entered the transmit interrupt if statement
+		UART0_DR_R = 'G';						// show 'G' on terminal too
 		// Acknowledge transmit receive interrupt
 		uart0_interrupt_clear_transmit();
 		
